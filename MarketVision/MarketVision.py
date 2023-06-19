55eb9be46f691d0e250ab690e33cbff9c6bd9413
Cavey <m.123051427@gmail.com>
2023-06-20 00:00:21 +0800

    modify getTarget function add data_display function add standardize function add runPCA function

--- MarketVision/MarketVision.py
+++ MarketVision/MarketVision.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 import yfinance as yf
 
 getData=yf.download
@@ -18,4 +21,22 @@ def getTarget(df,next):
 	choices=['Equal','Decreasing','Increasing']
 	df['target']=np.select(conditions,choices) 
 	df['target']=df['target'].shift(-next)
-	return df
+	df=df.dropna()
+	target=df['target'].values
+	df=df.drop(columns=['target'])
+	return {'data':df,'target':target}
+
+def data_display(data):
+	print(data,'\n',type(data))
+
+def standardize(X):
+	sc=StandardScaler()
+	sc.fit(X)
+	X_std=sc.transform(X)
+	return X_std
+
+def runPCA(X_train_std):
+	pca=PCA()
+	X_train_pca=pca.fit_transform(X_train_std)
+	return pca.explained_variance_ratio_
+	
